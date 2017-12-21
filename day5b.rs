e5c8e629e64d5eb5292e398ae6bcf5299fbddee9
Adrien Coulier <adrien.coulier@it.uu.se>
2017-12-21 16:14:41 +0100

    Solve day5b

---
+++ day5b.rs
@@ -0,0 +1,26 @@
+fn main() {
+    use std::io::{self, BufRead};
+
+    let stdin = io::stdin();
+
+    let mut list = Vec::new();
+    for number in stdin.lock().lines() {
+        list.push(number.unwrap().parse::<i32>().unwrap());
+    }
+
+    let mut current:i32 = 0;
+    let mut nsteps = 0;
+    while 0 <= current && current < list.len() as i32 {
+        nsteps += 1;
+        if list[current as usize] >= 3 {
+            list[current as usize] -= 1;
+            current += list[current as usize] + 1;
+        }
+        else {
+            list[current as usize] += 1;
+            current += list[current as usize] - 1;
+        }
+    }
+
+    println!("{}", nsteps);
+}
