70c0df24d8c78571a3afd69e80dc3d05d8fa83e6
Adrien Coulier <adrien.coulier@it.uu.se>
2019-12-05 08:30:00 +0100

    Refactor day5b

--- src/day5b.rs
+++ src/day5b.rs
@@ -1,3 +1,12 @@
+fn get_in(data:&Vec<i32>, pointer:usize) -> (usize, usize) {
+    let in1 = if data[pointer]/100 % 10 == 1 { pointer + 1 }
+        else { data[pointer+1] as usize };
+    let in2 = if data[pointer]/1000 % 10 == 1 { pointer + 2 }
+        else { data[pointer+2] as usize };
+
+    (in1, in2)
+}
+
 fn main() {
     use std::io::{self, BufRead};
 
@@ -25,20 +34,14 @@ fn main() {
         match out {
             Some(out) => numbers[out] = match opcode {
                 1 | 2 => {
-                    let in1 = if numbers[i]/100 % 10 == 1 { i + 1 }
-                        else { numbers[i+1] as usize };
-                    let in2 = if numbers[i]/1000 % 10 == 1 { i + 2 }
-                        else { numbers[i+2] as usize };
+                    let (in1, in2) = get_in(&numbers, i);
 
                     if opcode == 1 { numbers[in1] + numbers[in2]}
                         else { numbers[in1] * numbers[in2] }},
                 3 => { input },
                 4 => { println!("Opcode 4({}): {}", i, numbers[out]); numbers[out] },
                 7 | 8 => {
-                    let in1 = if numbers[i]/100 % 10 == 1 { i + 1 }
-                        else { numbers[i+1] as usize };
-                    let in2 = if numbers[i]/1000 % 10 == 1 { i + 2 }
-                        else { numbers[i+2] as usize };
+                    let (in1, in2) = get_in(&numbers, i);
 
                     if (opcode == 7 && numbers[in1] < numbers[in2])
                         || (opcode == 8 && numbers[in1] == numbers[in2])
@@ -55,10 +58,7 @@ fn main() {
             1 | 2 | 7 | 8 => i + 4,
             3 | 4 => i + 2,
             5 | 6 => {
-                let in1 = if numbers[i]/100 % 10 == 1 { i + 1 }
-                    else { numbers[i+1] as usize };
-                let in2 = if numbers[i]/1000 % 10 == 1 { i + 2 }
-                    else { numbers[i+2] as usize };
+                let (in1, in2) = get_in(&numbers, i);
 
                 if (opcode == 5 && numbers[in1] != 0) || (opcode == 6 && numbers[in1] == 0)
                     { numbers[in2] as usize }
