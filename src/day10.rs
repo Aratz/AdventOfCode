cc9aab59425bf44606fa147361c7cbea93950629
Adrien Coulier <adrien.coulier@it.uu.se>
2019-12-10 09:20:44 +0100

    Refactor day10

--- src/day10.rs
+++ src/day10.rs
@@ -3,7 +3,6 @@ extern crate num_integer;
 use std::io::{self, BufRead};
 use std::collections::HashSet;
 use num_integer::Integer;
-use std::cmp::max;
 
 fn main() {
     let stdin = io::stdin();
@@ -15,34 +14,25 @@ fn main() {
                   .filter_map(move |(x, c)| if c == '#' { Some((x as i32, y as i32)) } else { None }))
         .collect();
 
-    let mut max_count = 0;
+    let base = carta.iter().map(|candidate|
+             (
+                 carta.iter().filter(|&ast| ast != candidate)
+                 .filter_map(|asteroid| 
+                     {
+                         let (xx, yy) = (asteroid.0 - candidate.0, asteroid.1 - candidate.1);
+                         let p = xx.gcd(&yy);
+                         let step = (xx/p, yy/p);
 
-    for candidate in carta.iter() {
-        let mut count = 0;
+                         match (1..p).map(|p| (candidate.0 + p*step.0, candidate.1 + p*step.1))
+                             .filter(|c| carta.contains(&c)).count() {
+                                 0 => Some(asteroid),
+                                 _ => None,
+                             }
+                     }).count(),
+                     candidate,
+                     )
+            ).max().unwrap();
 
-        for asteroid in carta.iter() {
-            if candidate == asteroid { continue }
-
-            let (xx, yy) = (asteroid.0 - candidate.0, asteroid.1 - candidate.1);
-
-            let p = xx.gcd(&yy);
-
-            if p == 1 {
-                count += 1;
-                continue;
-            }
-
-            let step = (xx/p, yy/p);
-
-            count += match (1..p).map(|p| (candidate.0 + p*step.0, candidate.1 + p*step.1))
-                .filter(|c| carta.contains(&c)).next() {
-                    Some(_) => 0,
-                    None => 1
-                }
-        }
-
-        max_count = max(max_count, count);
-    }
-
-    println!("{}", max_count);
+    println!("Best possible location at {}, {} ({} asteroids detected)",
+        (base.1).0, (base.1).1, base.0);
 }
