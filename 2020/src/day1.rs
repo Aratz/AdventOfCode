e452d8b2f03aa49a27d0a9398e749fc5146b3014
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-01 19:41:18 +0100

    Solve day1

---
+++ 2020/src/day1.rs
@@ -0,0 +1,19 @@
+fn main() {
+    use std::io::{self, BufRead};
+
+    let stdin = io::stdin();
+
+    let numbers = stdin.lock().lines()
+        .map(|expense| expense.unwrap().parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    let target = 2020;
+
+    let (v1, v2) = numbers
+        .iter()
+        .flat_map(|v1| numbers.iter().clone().map(move |v2| (v1, v2)))
+        .filter(|(&v1, &v2)| v1 + v2 == target).next().unwrap();
+
+
+    println!("{}", v1*v2);
+}
