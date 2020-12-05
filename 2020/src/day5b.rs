8a72d9eb7c9f6760d5fb899fd28915dc99fc372d
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-05 10:37:31 +0100

    Solve day5b

---
+++ 2020/src/day5b.rs
@@ -0,0 +1,20 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut seats = stdin.lock().lines()
+        .map(|s| s.unwrap().chars().map(|c| match c {
+            'R' | 'B' => 1,
+            'F' | 'L' => 0,
+            _ => panic!("Code Error!"),
+        }).rev().enumerate().map(|(i, d)| d<<i).sum()).collect::<Vec<u32>>();
+
+    seats.sort();
+
+    let seats = seats;
+
+    let my_seat = seats.iter().zip(seats.iter().skip(1)).filter(|(&s0, &s1)| s0 != s1 - 1).next().unwrap();
+
+    println!("{:?}", my_seat.0+1);
+
+}
