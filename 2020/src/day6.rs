34cf00e1556b2f91e0c72087136685f0e7c9f677
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-06 11:26:12 +0100

    Solve day6

---
+++ 2020/src/day6.rs
@@ -0,0 +1,27 @@
+extern crate regex;
+
+use std::io::{self, Read};
+use regex::Regex;
+use std::collections::HashSet;
+
+fn main() {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    {
+        let mut stdin_lock = stdin.lock();
+        stdin_lock.read_to_string(&mut buffer).unwrap();
+    }
+
+    let re = Regex::new(r"([a-z]+(\s|$))+").unwrap();
+
+    let res: usize = re.captures_iter(&buffer)
+        .map(|c| c.iter().next().unwrap().unwrap().as_str()) // Extract match
+        .map(|forms| forms
+             .split("\n")
+             .flat_map(|form| form.chars())
+             .collect::<HashSet<_>>()
+             .len())
+        .sum();
+
+    println!("{}", res);
+}
