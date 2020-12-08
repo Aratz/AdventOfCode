13290eb462d34ac8c0245ee7586bd4879c726649
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-08 21:45:21 +0100

    Solve day22b (2019)

--- 2019/src/day22b.rs
+++ 2019/src/day22b.rs
@@ -1,20 +1,74 @@
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 
-const N:i64 = 119315717514047;
-const K: i64 = 101741582076661;
+const M:i128 = 119315717514047;
+const K: i128 = 101741582076661;
+
 
 use regex::Regex;
 use std::io::{self, BufRead};
 
-#[derive(Debug)]
-enum Technique {
-    DealStack,
-    Cut(i64),
-    DealInc(i64),
+fn mult_noverflow(a: i128, b: i128, m: i128) -> i128 {
+    let mut res = 0;
+    let mut a = a;
+    let mut b = b;
+
+    while b > 0 {
+        if b % 2 == 1 {
+            res = (res + a) % m;
+        }
+        a = (a *2 ) % m;
+        b = b / 2;
+    }
+
+    res
 }
 
-fn parse_action(action: &str) -> Technique {
+fn compose(f: (i128, i128), g: (i128, i128)) -> (i128, i128) {
+    (mult_noverflow(f.0, g.0, M), (mult_noverflow(f.0, g.1, M) + f.1) % M)
+}
+
+fn quick_exp(mut f_exp: (i128, i128), k: i128) -> (i128, i128) { // OK
+    let mut f = (1, 0);
+    let mut exp = 0;
+
+    while (1 << exp) <= k {
+        if K & (1 << exp) > 0 {
+            f = compose(f_exp, f);
+        }
+        f_exp = compose(f_exp, f_exp);
+        exp += 1;
+    }
+
+    f
+}
+
+fn xgcd(a: i128, b: i128) -> (i128, (i128, i128)) { // OK
+    let (mut old_r, mut r) = (a, b);
+    let (mut old_s, mut s) = (1, 0);
+    let (mut old_t, mut t) = (0, 1);
+
+    while r != 0 {
+        let quotient = old_r / r;
+
+        let temp_r = old_r;
+        old_r = r;
+        r = temp_r - mult_noverflow(quotient, r, b);
+
+        let temp_s = old_s;
+        old_s = s;
+        s = (temp_s - mult_noverflow(quotient, s, b) +b)%b ;
+
+        let temp_t = old_t;
+        old_t = t;
+        t = (temp_t - mult_noverflow(quotient, t, b) +b)%b;
+
+    }
+
+    (old_r, (old_s, old_t))
+}
+
+fn parse_action(action: &str) -> (i128, i128) {
     lazy_static! {
         static ref TECH: [Regex; 3] = [
             Regex::new(r"^deal into new stack$").unwrap(),
@@ -26,9 +80,14 @@ fn parse_action(action: &str) -> Technique {
     for (i, re) in TECH.iter().enumerate() {
         if let Some(cap) = re.captures(action) {
             match i {
-                0 => return Technique::DealStack,
-                1 => return Technique::Cut(cap.get(1).unwrap().as_str().parse().unwrap()),
-                2 => return Technique::DealInc(cap.get(1).unwrap().as_str().parse().unwrap()),
+                0 => return (M-1, M-1), // OK
+                1 => return (
+                    1,
+                    (M + cap.get(1).unwrap().as_str().parse::<i128>().unwrap()) % M), // OK
+                2 => {
+                    let n = cap.get(1).unwrap().as_str().parse::<i128>().unwrap();
+                    return (((xgcd(n, M).1).0 + M) % M, 0) // OK
+                },
                 _ => continue,
             }
         }
@@ -37,44 +96,17 @@ fn parse_action(action: &str) -> Technique {
     panic!("No match found!")
 }
 
-fn mult_noverflow(a: i64, b: i64) -> i64 {
-    let mut res = 0;
-    let mut a = a;
-    let mut b = b;
-
-    while b > 0 {
-        if b % 2 == 1 {
-            res = (res + a) % N;
-        }
-        a = (a *2 ) % N;
-        b = b / 2;
-    }
-
-    res
-}
-
 fn main() {
-    let k = N - K;
-
     let stdin = io::stdin();
 
-    let process: Vec<Technique> = stdin.lock().lines()
+    let process: Vec<(i128, i128)> = stdin.lock().lines()
         .map(|l| parse_action(&l.unwrap())).collect();
 
-    let mut i = 2020;
+    let f_exp = process.iter().fold((1, 0), |acc, &x| {
+        compose(acc, x)
+    });
+    let i = 2020;
+    let f = quick_exp(f_exp, K);
 
-    for l in 0..k {
-        if l%1000000 == 0 {
-            println!("{}", l);
-        }
-        for p in process.iter() {
-            match p {
-                Technique::DealStack => { i = N - i - 1; }, // OK
-                Technique::Cut(n) => { i = (N + i - n)%N; }, // OK
-                Technique::DealInc(n) => { i = mult_noverflow(i, *n); }, // OK
-            }
-        }
-    }
-
-    println!("{:?}", i);
+    println!("{:?}", (mult_noverflow(i, f.0, M) + f.1) % M);
 }
