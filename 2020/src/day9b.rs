73363bbe739ea3072619d554787a371bfd826639
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-09 10:17:38 +0100

    Solve day9b

---
+++ 2020/src/day9b.rs
@@ -0,0 +1,37 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let p = 25;
+
+    let stdin = io::stdin();
+
+    let numbers: Vec<i64> = stdin.lock().lines()
+        .map(|s| s.unwrap().parse().unwrap())
+        .collect();
+
+    let n_inv = numbers[p..].iter().enumerate()
+        .filter(|&(i, n)| {
+            numbers[i..i+p].iter()
+                .flat_map(|a| numbers[i..i+p].iter().clone().map(move |b| (a, b)))
+                .filter(|(a, b)| a != b)
+                .map(|(a, b)| a + b)
+                .filter(|&s| s == *n)
+                .count() == 0
+        }).map(|(i, _)| numbers[i+p]).next().unwrap();
+
+    let mut cum_sum: Vec<i64> = numbers.iter().scan(0, |state, &x| {
+            *state = *state + x;
+            Some(*state)
+        }).collect();
+    cum_sum.insert(0, 0);
+    //remember indices are shifted by 1
+
+    let (i, j) = (0..cum_sum.len()).flat_map(|i| (0..cum_sum.len()).map(move |j| (i, j)))
+        .filter(|&(i, j)| j > i)
+        .filter(|&(i, j)| cum_sum[j] - cum_sum[i] == n_inv)
+        .next().unwrap();
+
+    let res = numbers[i..j].iter().min().unwrap() + numbers[i..j].iter().max().unwrap();
+
+    println!("{}", res);
+}
