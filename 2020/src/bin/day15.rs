3797465fd6b8665780439eeab286e0d88ffe25f3
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-15 16:43:03 +0100

    Solve day15b

--- 2020/src/bin/day15.rs
+++ 2020/src/bin/day15.rs
@@ -1,30 +1,40 @@
 use std::io::{self, BufRead};
-use std::collections::HashMap;
+
+mod day15 {
+    use std::collections::HashMap;
+
+    pub fn solve(n: usize, numbers: &Vec<usize>) -> usize {
+        let mut numbers = numbers.clone();
+        let mut last_spoken: HashMap<usize, (Option<usize>, Option<usize>)> = HashMap::new();
+
+        for i in 0..numbers.len() {
+            last_spoken.insert(numbers[i], (Some(i+1), None));
+        }
+
+        for i in numbers.len()..n {
+            let ith_spoken = match last_spoken.get(&numbers[i-1]).unwrap() {
+                (Some(i1), Some(i2)) => { i1 - i2 },
+                (Some(_), None) => { 0 },
+                _ => { panic!("Malformed last spoken map!"); },
+            };
+
+            numbers.push(ith_spoken);
+            last_spoken.entry(ith_spoken)
+                .and_modify(|e| *e =  (Some(i+1), e.0))
+                .or_insert((Some(i+1), None));
+        }
+
+        *numbers.last().unwrap()
+    }
+}
 
 fn main() {
     let stdin = io::stdin();
 
-    let mut numbers: Vec<usize> = stdin.lock().lines().next().unwrap().unwrap()
+    let numbers: Vec<usize> = stdin.lock().lines().next().unwrap().unwrap()
         .split(",").map(|n| n.parse().unwrap()).collect();
 
-    let mut last_spoken: HashMap<usize, (Option<usize>, Option<usize>)> = HashMap::new();
 
-    for i in 0..numbers.len() {
-        last_spoken.insert(numbers[i], (Some(i+1), None));
-    }
-
-    for i in numbers.len()..2020 {
-        let ith_spoken = match last_spoken.get(&numbers[i-1]).unwrap() {
-            (Some(i1), Some(i2)) => { i1 - i2 },
-            (Some(_), None) => { 0 },
-            _ => { panic!("Malformed last spoken map!"); },
-        };
-
-        numbers.push(ith_spoken);
-        last_spoken.entry(ith_spoken)
-            .and_modify(|e| *e =  (Some(i+1), e.0))
-            .or_insert((Some(i+1), None));
-    }
-
-    println!("{:?}", numbers.last().unwrap());
+    println!("Solution A-part: {}", day15::solve(2020, &numbers));
+    println!("Solution B-part: {}", day15::solve(30000000, &numbers));
 }
