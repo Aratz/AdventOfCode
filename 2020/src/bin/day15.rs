a93c42a1ebd7e987ba728b78d10863b44f7e2198
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-15 21:51:11 +0100

    Add benchmark

--- 2020/src/bin/day15.rs
+++ 2020/src/bin/day15.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 use std::io::{self, BufRead};
 
 mod day15 {
@@ -42,6 +46,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test::Bencher;
 
     #[test]
     fn test_solve_2020() {
@@ -65,5 +70,10 @@ mod tests {
         assert_eq!(day15::solve(30_000_000, &vec![2, 3, 1]), 6_895_259);
         assert_eq!(day15::solve(30_000_000, &vec![3, 2, 1]),        18);
         assert_eq!(day15::solve(30_000_000, &vec![3, 1, 2]),       362);
+    }
+
+    #[bench]
+    fn bench_solve_1e5(b: &mut Bencher) {
+        b.iter(|| day15::solve(300_000, &vec![0, 3, 6]));
     }
 }
