7d955e051fbca803509ce3534855726d530bab6b
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-20 19:20:53 +0100

    Solve day20b

--- 2020/src/bin/day20.rs
+++ 2020/src/bin/day20.rs
@@ -81,9 +81,9 @@ mod day20 {
             self.borders.w = self.borders.w.chars().rev().collect();
             self.borders.s = self.borders.s.chars().rev().collect();
             self.borders.e = self.borders.e.chars().rev().collect();
-            //for row in self.content.iter() {
-            //    row.reverse();
-            //}
+            for row in self.content.iter_mut() {
+                row.reverse();
+            }
         }
 
         fn flip_v(&mut self) {
@@ -92,7 +92,7 @@ mod day20 {
             self.borders.w = self.borders.w.chars().rev().collect();
             self.borders.s = self.borders.s.chars().rev().collect();
             self.borders.e = self.borders.e.chars().rev().collect();
-            //self.content.reverse();
+            self.content.reverse();
         }
 
         fn rotate90(&mut self) {
@@ -101,6 +101,15 @@ mod day20 {
             let s = mem::replace(&mut self.borders.s, w);
             let e = mem::replace(&mut self.borders.e, s);
             self.borders.n = e;
+
+            let mut rotated = self.content.clone();
+            for y in 0..SIDE_LENGTH {
+                for x in 0..SIDE_LENGTH {
+                    rotated[SIDE_LENGTH - x -1][y] = self.content[y][x];
+                }
+            }
+
+            self.content = rotated
         }
 
         fn rotate180(&mut self) {
@@ -128,22 +137,25 @@ mod day20 {
         tiles: HashMap<u64, Tile>,
         border_pool: HashMap<String, (Side, u64)>,
         assembled: HashMap<u64, (i32, i32)>,
-        queue: VecDeque<Tile>,
+        queue: VecDeque<u64>,
         done: bool,
     }
 
     impl AssemblyLine {
         fn new(tiles: &Vec<Tile>) -> Self {
+            let tiles: HashMap<u64, Tile> = tiles.iter().map(|t| (t.id, t.clone())).collect();
+            let queue = VecDeque::from(tiles.keys().map(|k| *k).collect::<Vec<u64>>());
             Self {
-                tiles: tiles.iter().map(|t| (t.id, t.clone())).collect(),
+                tiles,
                 border_pool: HashMap::new(),
                 assembled: HashMap::new(),
-                queue: VecDeque::from(tiles.clone()),
+                queue,
                 done: false,
             }
         }
 
-        fn find_match(&mut self, new_tile: &mut Tile) -> bool {
+        fn find_match(&mut self, new_tile_id: u64) -> bool {
+            let new_tile = self.tiles.get_mut(&new_tile_id).unwrap();
             for (new_side, border) in new_tile.borders.clone().into_iter() {
                 match self.border_pool.get(&border.chars().rev().collect::<String>()) {
                     Some((fixed_side, id)) => { //Set rotation, set coordinates, add to pool, then continue
@@ -195,22 +207,54 @@ mod day20 {
                 return
             }
 
-            let first_tile = self.queue.pop_front().unwrap();
+            let first_tile = &self.tiles[&self.queue.pop_front().unwrap()];
             self.assembled.insert(first_tile.id, (0, 0));
-            for (s, border) in first_tile.borders.into_iter() {
+            for (s, border) in first_tile.borders.clone().into_iter() {
                 self.border_pool.insert(border.into(), (s, first_tile.id));
             }
 
-            while let Some(mut new_tile) = self.queue.pop_front() {
-                if !self.find_match(&mut new_tile)
-                        && !self.find_match({ new_tile.flip_h(); &mut new_tile})
-                        && !self.find_match({ new_tile.flip_h(); new_tile.flip_v(); &mut new_tile}) {
-                    new_tile.flip_v();
-                    self.queue.push_back(new_tile);
+            while let Some(new_tile_id) = self.queue.pop_front() {
+                if !self.find_match(new_tile_id) {
+                    self.tiles.get_mut(&new_tile_id).unwrap().flip_h();
+                    if !self.find_match(new_tile_id) {
+                        self.tiles.get_mut(&new_tile_id).unwrap().flip_h();
+                        self.tiles.get_mut(&new_tile_id).unwrap().flip_v();
+                        if !self.find_match(new_tile_id) {
+                            self.tiles.get_mut(&new_tile_id).unwrap().flip_v();
+                            self.queue.push_back(new_tile_id);
+                        }
+                    }
                 }
             }
 
             self.done = true;
+        }
+
+        fn get_full_picture(&mut self) -> Vec<Vec<char>> {
+            self.assemble();
+
+            let image = &self.assembled;
+            let min_x = image.values().map(|v| v.0).min().unwrap();
+            let max_x = image.values().map(|v| v.0).max().unwrap();
+            let min_y = image.values().map(|v| v.1).min().unwrap();
+            let max_y = image.values().map(|v| v.1).max().unwrap();
+
+
+            // assemble full picture
+            let rev_map: HashMap<(i32, i32), u64> = image.iter().map(|(k, v)| (*v, *k)).collect();
+            let mut full_picture: Vec<Vec<char>> = Vec::new();
+
+            for y in min_y..=max_y {
+                for i in (1..SIDE_LENGTH-1).rev() {
+                    full_picture.push(Vec::new());
+                    for x in min_x..=max_x {
+                        full_picture.last_mut().unwrap().append(
+                            &mut self.tiles[&rev_map[&(x, y)]].content[i][1..SIDE_LENGTH-1].to_vec());
+                    }
+                }
+            }
+
+            full_picture
         }
     }
 
@@ -230,6 +274,80 @@ mod day20 {
              || image[k] == (min_x, max_y)
              || image[k] == (max_x, min_y)
              || image[k] == (max_x, max_y)).product::<u64>()
+    }
+
+    fn rotate90(pattern: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let w = pattern[0].len();
+
+        (0..w).rev().map(|i| pattern.iter().map(|row| row[i]).collect()).collect()
+    }
+
+    fn flip_h(mut pattern: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        for row in pattern.iter_mut() {
+            row.reverse();
+        }
+
+        pattern
+    }
+
+    fn flip_v(mut pattern: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        pattern.reverse();
+
+        pattern
+    }
+
+
+    pub fn solve_b(tiles: &Vec<Tile>) -> usize {
+        let mut assembly_line = AssemblyLine::new(tiles);
+        let full_picture = assembly_line.get_full_picture();
+        let pattern = vec![
+            "                  # ".chars().collect(),
+            "#    ##    ##    ###".chars().collect(),
+            " #  #  #  #  #  #   ".chars().collect(),
+        ];
+
+        let patterns = vec![
+            pattern.clone(),
+            rotate90(pattern.clone()),
+            rotate90(rotate90(pattern.clone())),
+            rotate90(rotate90(rotate90(pattern.clone()))),
+            flip_h(pattern.clone()),
+            rotate90(flip_h(pattern.clone())),
+            rotate90(rotate90(flip_h(pattern.clone()))),
+            rotate90(rotate90(rotate90(flip_h(pattern.clone())))),
+            flip_v(pattern.clone()),
+            rotate90(flip_v(pattern.clone())),
+            rotate90(rotate90(flip_v(pattern.clone()))),
+            rotate90(rotate90(rotate90(flip_v(pattern.clone())))),
+        ];
+
+        // Search for 2D pattern and nullify
+        let n_match = patterns.iter().map(|pattern| {
+            let h_pic = full_picture.len();
+            let w_pic = full_picture[0].len();
+
+            let h_pat = pattern.len();
+            let w_pat = pattern[0].len();
+
+            let origins: Vec<(usize, usize)> = (0..h_pic - h_pat + 1).flat_map(|i_pic|
+                (0..w_pic - w_pat + 1).map(move |j_pic| (i_pic, j_pic))).collect();
+
+            origins.iter().filter(
+                |(i_pic, j_pic)| {
+                    let pat_px: Vec<(usize, usize)> = (0..h_pat).flat_map(|i_pat|
+                        (0..w_pat).map(move |j_pat| (i_pat, j_pat))).collect();
+
+                    pat_px.iter().filter(
+                        |(i_pat, j_pat)| pattern[*i_pat][*j_pat] == ' '
+                            || pattern[*i_pat][*j_pat] == full_picture[i_pic+i_pat][j_pic+j_pat]
+                            ).count() == h_pat * w_pat
+                }).count()
+        }).max().unwrap();
+
+        let total_pic: usize = full_picture.iter().map(|row| row.iter().filter(|&&c| c=='#').count()).sum();
+        let total_path: usize = pattern.iter().map(|row| row.iter().filter(|&&c| c=='#').count()).sum();
+
+        total_pic - n_match * total_path
     }
 }
 
@@ -253,4 +371,5 @@ fn main() {
             )).collect();
 
     println!("Solution A-part: {}", day20::solve_a(&tiles));
+    println!("Solution B-part: {}", day20::solve_b(&tiles));
 }
