654db67ec1e9ded4ef08f696c8a8e391305c71ba
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-23 14:01:22 +0100

    Optimize day23
    Realized I could just as well use a simple Vector.

--- 2020/src/bin/day23.rs
+++ 2020/src/bin/day23.rs
@@ -1,8 +1,8 @@
 mod day23 {
-    use std::collections::{VecDeque, HashMap};
+    use std::collections::VecDeque;
 
-    fn iter_circle(mut cups: usize, n_moves: usize, n_cups: usize) -> HashMap<usize, usize> {
-        let mut circle: HashMap<usize, usize> = HashMap::new();
+    fn iter_circle(mut cups: usize, n_moves: usize, n_cups: usize) -> Vec<usize> {
+        let mut circle: Vec<usize> = vec![0; n_cups + 1];
         let mut staging = VecDeque::new();
         let base = 10;
 
@@ -10,19 +10,19 @@ mod day23 {
         let mut last_digit = 10;
         let first_digit = cups % base;
         while cups != 0 {
-            circle.insert(cups % base, last_digit);
+            circle[cups % base] = last_digit;
             last_digit = cups % base;
             cups /= base;
         }
         if n_cups == 9 {
-            circle.insert(first_digit, last_digit);
+            circle[first_digit] = last_digit;
         }
         else {
-            circle.insert(n_cups, last_digit);
+            circle[n_cups] = last_digit;
         }
 
         for i in 10..n_cups {
-            circle.insert(i, i + 1);
+            circle[i] = i + 1;
         }
 
 
@@ -35,33 +35,25 @@ mod day23 {
         //  - pop and stack next three and adjust dest
             for _ in 0..3 {
                 //source.next = last
-                staging.push_back(circle[&source]);
-                let source_next = circle[&circle[&source]];
-                circle.insert(source, source_next);
+                staging.push_back(circle[source]);
+                let source_next = circle[circle[source]];
+                circle[source] = source_next;
             }
             while staging.iter().any(|&v| v == dest) {
                 dest = (dest - 1 + base - 1) % base + 1;
             }
         //  - push back cups
-            let mut dest_last = circle[&dest];
+            let dest_last = circle[dest];
             let mut prev = dest;
             while let Some(v) = staging.pop_front() {
-                circle.insert(prev, v);
+                circle[prev] = v;
                 prev = v;
             }
-            circle.insert(prev, dest_last);
+            circle[prev] = dest_last;
 
         // Step forward
-            source = circle[&source];
+            source = circle[source];
         }
-
-        //let mut dbg_vec = Vec::new();
-        //let mut source = 3;
-        //for _ in 0..20 {
-        //    source = circle[&source];
-        //    dbg_vec.push(source);
-        //}
-        //dbg!(&dbg_vec);
 
         circle
     }
@@ -69,17 +61,17 @@ mod day23 {
     pub fn solve_a(cups: usize, n_moves: usize) -> usize {
         let circle = iter_circle(cups, n_moves, 9);
         let mut res = Vec::new();
-        let mut source = circle[&1];
+        let mut source = circle[1];
         while source != 1 {
             res.push(source);
-            source = circle[&source];
+            source = circle[source];
         }
         res.iter().rev().enumerate().map(|(i, v)| v*10usize.pow(i as u32)).sum::<usize>()
     }
 
     pub fn solve_b(cups: usize, n_moves: usize, n_cups: usize) -> usize {
         let circle = iter_circle(cups, n_moves, n_cups);
-        circle[&1] * circle[&circle[&1]]
+        circle[1] * circle[circle[1]]
     }
 
     #[cfg(test)]
