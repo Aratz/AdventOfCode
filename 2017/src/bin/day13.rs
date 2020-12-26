f00d68a30d89561a362e18c52dc5ca85e272c0bc
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-26 07:50:11 +0100

    Solve day13 (2017)

---
+++ 2017/src/bin/day13.rs
@@ -0,0 +1,36 @@
+mod day13 {
+    use std::collections::HashMap;
+
+    pub fn solve_a(layers: &HashMap<usize, usize>) -> usize {
+        layers.iter().filter(|(depth, range)| *depth % (2 * (*range - 1)) == 0)
+            .map(|(depth, range)| depth * range)
+            .sum()
+    }
+
+    #[cfg(test)]
+    mod test_day13 {
+        use super::*;
+
+        #[test]
+        fn test_solve_a() {
+            let layers = vec![(0, 3), (1,2), (4, 4), (6, 4)].into_iter().collect();
+
+            assert_eq!(solve_a(&layers), 24);
+        }
+    }
+}
+
+fn main() {
+    use std::io::{self, BufRead};
+
+    let stdin = io::stdin();
+
+    let firewall = stdin.lock().lines().map(
+        |l| {
+            let dr: Vec<_> = l.unwrap().split(": ").map(|v| v.parse().unwrap())
+                .collect();
+            (dr[0], dr[1])
+        }).collect();
+
+    println!("Solution A-part: {}", day13::solve_a(&firewall));
+}
