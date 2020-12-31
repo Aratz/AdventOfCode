13b464c11c4bdfde13a37ecf34730fcab4ba3ebc
Adrien Coulier <adrien.coulier@it.uu.se>
2020-12-31 12:09:32 +0100

    Solve day17b (2017)

--- 2017/src/bin/day17.rs
+++ 2017/src/bin/day17.rs
@@ -18,6 +18,27 @@ mod day17 {
         spinlock[2017]
     }
 
+    pub fn solve_b(step: usize) -> usize {
+        let n_ins = 50_000_000;
+        let mut next = 0;
+
+        let mut pos = 0;
+        let mut spin_size = 1;
+
+        for n in 1..=n_ins {
+            pos = (pos + step) % spin_size;
+
+            if pos == 0 {
+                next = n;
+            }
+
+            spin_size += 1;
+            pos += 1;
+        }
+
+        next
+    }
+
     #[cfg(test)]
     mod test_day17 {
         use super::*;
@@ -37,6 +58,6 @@ fn main() {
     let step = stdin.lock().lines().next().unwrap().unwrap()
         .parse().unwrap();
 
-    println!("Solution A-part: {}", day17::solve_a(step))
-
+    println!("Solution A-part: {}", day17::solve_a(step));
+    println!("Solution B-part: {}", day17::solve_b(step));
 }
