c928165587f115f27ad5c753f11e1bf49167e697
Adrien Coulier <adrien.coulier@it.uu.se>
2021-11-01 18:43:23 +0100

    Solve day07b (2016)

--- 2016/src/bin/day07.rs
+++ 2016/src/bin/day07.rs
@@ -30,12 +30,34 @@ mod day07 {
             self.no_bracket.iter().any(|s| is_abba(s))
                 && !self.bracket.iter().any(|s| is_abba(s))
         }
+
+        #[allow(non_snake_case)]
+        fn is_SSL(&self) -> bool {
+            let abas: Vec<String> = self.no_bracket.iter()
+                .map(|s| get_abas(s))
+                .flatten()
+                .collect();
+            self.bracket.iter().any(|s| abas.iter().any(|aba| is_bab(s, aba)))
+        }
     }
 
     fn is_abba(s: &str) -> bool {
         s.chars().zip(s.chars().skip(1)).zip(s.chars().skip(2).zip(s.chars().skip(3)))
             .filter(|((a1, b1), (b2, a2))| a1 == a2 && b1 == b2 && a1 != b1)
             .next().is_some()
+    }
+
+    fn get_abas(s: &str) -> Vec<String> {
+        s.chars().zip(s.chars().skip(1).zip(s.chars().skip(2)))
+            .filter(|(a1, (b, a2))| a1 == a2 && a1 != b)
+            .map(|(a1, (b, a2))| vec![a1, b, a2].iter().collect())
+            .collect()
+    }
+
+    fn is_bab(s: &str, aba: &str) -> bool {
+        let v_aba: Vec<_> = aba.chars().collect();
+        let bab: String = vec![v_aba[1], v_aba[0], v_aba[1]].iter().collect();
+        s.find(&bab).is_some()
     }
 
     fn parse_ips(lines: &Vec<String>) -> Vec<Ipv7> {
@@ -47,6 +69,12 @@ mod day07 {
     pub fn solve_a(lines: &Vec<String>) -> usize {
         parse_ips(lines).iter()
             .filter(|ip| ip.is_TLS())
+            .count()
+    }
+
+    pub fn solve_b(lines: &Vec<String>) -> usize {
+        parse_ips(lines).iter()
+            .filter(|ip| ip.is_SSL())
             .count()
     }
 
@@ -64,6 +92,16 @@ mod day07 {
             assert!(!Ipv7::new("aaaa[qwer]tyui").is_TLS());
             assert!(!Ipv7::new("ioxxoj[asdfgh]zxcvbn[aunallaie]aunetau").is_TLS());
         }
+
+        #[test]
+        fn test_is_SSL() {
+            assert!(Ipv7::new("aba[bab]xyz").is_SSL());
+            assert!(Ipv7::new("aaa[kek]eke").is_SSL());
+            assert!(Ipv7::new("zazbz[bzb]cdb").is_SSL());
+            assert!(Ipv7::new("xyx[xyx]xyx[yxy]auieaue").is_SSL());
+
+            assert!(!Ipv7::new("xyx[xyx]xyx").is_SSL());
+        }
     }
 
 }
@@ -78,4 +116,5 @@ fn main() {
         .collect();
 
     println!("Solution A-part: {}", day07::solve_a(&lines));
+    println!("Solution B-part: {}", day07::solve_b(&lines));
 }
