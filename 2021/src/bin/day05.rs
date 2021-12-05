579faff1e4ddb998f85337b8be7f4c079d5bc35d
Adrien Coulier <adrien.coulier@it.uu.se>
2021-12-05 08:02:25 +0100

    Refactor day05

--- 2021/src/bin/day05.rs
+++ 2021/src/bin/day05.rs
@@ -2,40 +2,22 @@ mod day05 {
     use std::collections::HashMap;
     use std::cmp::{min, max};
 
-    pub fn solve_a(vents: &[((i32, i32), (i32, i32))]) -> usize {
-        let mut map: HashMap<(i32, i32), usize> = HashMap::new();
-
-        for &((x1, y1), (x2, y2)) in vents {
-            if x1 != x2 && y1 != y2 { continue; }
-
-            let ((x1, y1), (x2, y2)) = (
-                (min(x1, x2), min(y1, y2)),
-                (max(x1, x2), max(y1, y2))
-            );
-            for x in x1..=x2 {
-                for y in y1..=y2 {
-                    *map.entry((x, y)).or_default() += 1;
-                }
-            }
-        }
-
-        map.values().filter(|&&v| v >= 2).count()
-    }
-
-    pub fn solve_b(vents: &[((i32, i32), (i32, i32))]) -> usize {
+    pub fn solve_ab(vents: &[((i32, i32), (i32, i32))], diag: bool) -> usize {
         let mut map: HashMap<(i32, i32), usize> = HashMap::new();
 
         for &((x1, y1), (x2, y2)) in vents {
             if x1 != x2 && y1 != y2 {
-                let dx = if x1 < x2 { 1 } else { -1 };
-                let dy = if y1 < y2 { 1 } else { -1 };
+                if diag {
+                    let dx = if x1 < x2 { 1 } else { -1 };
+                    let dy = if y1 < y2 { 1 } else { -1 };
 
-                let (mut x, mut y) = (x1, y1);
-                while x != x2 {
+                    let (mut x, mut y) = (x1, y1);
+                    while x != x2 {
+                        *map.entry((x, y)).or_default() += 1;
+                        x += dx; y += dy;
+                    }
                     *map.entry((x, y)).or_default() += 1;
-                    x += dx; y += dy;
                 }
-                *map.entry((x, y)).or_default() += 1;
             }
             else {
                 let ((x1, y1), (x2, y2)) = (
@@ -67,6 +49,6 @@ fn main() {
         .map(|v| ((v[0], v[1]), (v[2], v[3])))
         .collect();
 
-    println!("Solution A-part: {}", day05::solve_a(&vents));
-    println!("Solution B-part: {}", day05::solve_b(&vents));
+    println!("Solution A-part: {}", day05::solve_ab(&vents, false));
+    println!("Solution B-part: {}", day05::solve_ab(&vents, true));
 }
