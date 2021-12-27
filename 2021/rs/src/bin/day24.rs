5fc881043f5b77eaafcd8367a604c05c6f24a68e
Adrien Coulier <adrien.coulier@it.uu.se>
2021-12-27 14:45:46 +0100

    Solve day24

---
+++ 2021/rs/src/bin/day24.rs
@@ -0,0 +1,59 @@
+mod day24 {
+    const BLOCK_SIZE: usize = 18;
+    const N_BLOCKS: usize = 14;
+
+    fn split_input(input: &[String]) -> Vec<Vec<String>> {
+        (0..N_BLOCKS)
+            .map(|i| input[BLOCK_SIZE*i..BLOCK_SIZE*(i+1)].to_vec())
+            .collect()
+    }
+
+    fn extract_abc(block: &[String]) -> (i64, i64, i64) {
+        (
+            block[5].split(' ').last().unwrap().parse().unwrap(),
+            block[15].split(' ').last().unwrap().parse().unwrap(),
+            block[4].split(' ').last().unwrap().parse().unwrap(),
+        )
+    }
+
+    pub fn solve(prgm: &[String], ab: bool) -> i64 {
+        let abc: Vec<_> = split_input(prgm)
+            .into_iter()
+            .map(|block| extract_abc(&block))
+            .collect();
+
+        let mut input = [0; 14];
+
+        let mut stack = Vec::new();
+        for (i, (a, b, c)) in abc.into_iter().enumerate() {
+            if c == 1 {
+                stack.push((i, b));
+            }
+            else {
+                let (i_w, b) = stack.pop().unwrap();
+                let w01 = (1..=9)
+                    .map(|w| (w, w + b + a))
+                    .filter(|&(_, w)| 1 <= w && w <= 9)
+                    .collect::<Vec<_>>();
+
+                input[i_w] = w01[if ab { 0 } else { w01.len() - 1 }].0;
+                input[i] = w01[if ab { 0 } else { w01.len() - 1 }].1;
+            }
+        }
+
+        input.iter().rev().enumerate()
+            .map(|(i, w)| w*10i64.pow(i as u32))
+            .sum::<i64>()
+    }
+}
+
+fn main() {
+    use std::io::{self, BufRead};
+
+    let stdin = io::stdin();
+
+    let input: Vec<String> = stdin.lock().lines().map(|l| l.unwrap()).collect();
+
+    println!("Solution A-part: {}", day24::solve(&input, false));
+    println!("Solution B-part: {}", day24::solve(&input, true));
+}
