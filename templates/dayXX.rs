97a687b1dd62e2367b410c04abc5c7ae33b8d452
Adrien Coulier <adrien.coulier@it.uu.se>
2021-12-29 17:51:37 +0100

    Add rust template

---
+++ templates/dayXX.rs
@@ -0,0 +1,32 @@
+mod dayXX {
+    fn solve_a(input: &str) -> {
+
+    }
+
+    #[cfg(test)]
+    mod test_dayXX {
+        use super::*;
+
+        #[test]
+        fn test_solve_a() {
+
+        }
+    }
+
+}
+
+fn main() {
+    use std::io::{self, Read};
+
+    let stdin = io::stdin();
+
+    let mut buffer = String::new();
+
+    {
+        let mut stdin_lock = stdin.lock();
+        stdin_lock.read_to_string(&mut buffer).unwrap();
+    }
+
+    println!("Solution A-part: {}", dayXX::solve_a(&buffer));
+    println!("Solution B-part: {}", dayXX::solve_b(&buffer));
+}
